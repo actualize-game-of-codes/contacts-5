c1c0f3d959028f52604914758b3126e8d81af2e1
Peter Jang <peter.x.jang@gmail.com>
2017-07-31 20:51:17 -0500

    Add validations to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ApplicationRecord
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true
+
   belongs_to :user
   has_many :contact_groups
   has_many :groups, :through => :contact_groups
